ce1081de75ab76acf6cf9a70f5debe985f6b6892
Yoga Prianjaya <yogaprianjaya@gmail.com>
2021-05-21 21:05:10 +0700

    update non-formal-education

--- js/dataJs/dataProfile.js
+++ js/dataJs/dataProfile.js
@@ -1,32 +1,47 @@
 const formalEducation = [
-    {
-      "id": 1,
-      "head": "Sekolah Dasar",
-      "title": "SD Negeri Waas 1",
-      "year": "2006 - 2012",
-      "description": "Menyelesaikan pendidikan Sekolah Dasar di SD Negeri Waas 1 secara tepat waktu yaitu selama 6 tahun"
-    },
-    {
-      "id": 2,
-      "head": "Sekolah Menengah Pertama",
-      "title": "SMP Negeri 1 Pameungpeuk",
-      "year": "2012 - 2015",
-      "description": "Menyelesaikan pendidikan Sekolah Menengah Pertama di SMP Negeri 1 Pameungpeuk secara tepat waktu yaitu selama 3 tahun"
-    },
-    {
-      "id": 3,
-      "head": "Sekolah Menengah Atas",
-      "title": "SMK Negeri 2 Baleendah",
-      "year": "2015 - 2018",
-      "description": "Menyelesaikan pendidikan Sekolah Menengah Atas di SMK Negeri 2 Baleendah Jurusan Teknik Komputer & Jaringan secara tepat waktu yaitu selama 3 tahun"
-    },
-    {
-      "id": 4,
-      "head": "Diploma 3",
-      "title": "D3 Teknologi Kompputer",
-      "year": "2018 - Sekarang",
-      "description": "Sedang mengambil kuliah di Program Study D3 Teknologi Komputer, Fakultas Ilmu Terapan, Telkom University"
-    }
+  {
+    "id": 1,
+    "head": "Sekolah Dasar",
+    "title": "SD Negeri Waas 1",
+    "year": "2006 - 2012",
+    "description": "Menyelesaikan pendidikan Sekolah Dasar di SD Negeri Waas 1 secara tepat waktu yaitu selama 6 tahun"
+  },
+  {
+    "id": 2,
+    "head": "Sekolah Menengah Pertama",
+    "title": "SMP Negeri 1 Pameungpeuk",
+    "year": "2012 - 2015",
+    "description": "Menyelesaikan pendidikan Sekolah Menengah Pertama di SMP Negeri 1 Pameungpeuk secara tepat waktu yaitu selama 3 tahun"
+  },
+  {
+    "id": 3,
+    "head": "Sekolah Menengah Atas",
+    "title": "SMK Negeri 2 Baleendah",
+    "year": "2015 - 2018",
+    "description": "Menyelesaikan pendidikan Sekolah Menengah Atas di SMK Negeri 2 Baleendah Jurusan Teknik Komputer & Jaringan secara tepat waktu yaitu selama 3 tahun"
+  },
+  {
+    "id": 4,
+    "head": "Diploma 3",
+    "title": "D3 Teknologi Kompputer",
+    "year": "2018 - Sekarang",
+    "description": "Sedang mengambil kuliah di Program Study D3 Teknologi Komputer, Fakultas Ilmu Terapan, Telkom University"
+  }
+]
+
+const nonFormalEducation = [
+  {
+    "id": 1,
+    "head": "Dicoding",
+    "title": "Belajar Dasar Pemrograman Web",
+    "year": "Agustus - 2020"
+  },
+  {
+    "id": 2,
+    "head": "Dicoding",
+    "title": "Belajar Membuat Front-End Web untuk pemula",
+    "year": "Mei - 2021"
+  }
 ]
 
 const experience = [
--- js/profile.js
+++ js/profile.js
@@ -3,10 +3,10 @@ $(document).ready(function (){
         const formalEducationCard = document.getElementById('formal-education-card');
         formalEducation.map(data => {
             const container = document.createElement('div');
-            container.setAttribute('class', 'col-3');
+            container.setAttribute('class', 'col-sm-3');
 
             const containerCard = document.createElement('div');
-            containerCard.setAttribute('class', 'card text-white bg-success mb-3');
+            containerCard.setAttribute('class', 'card text-white bg-success mb-3 h-100');
 
             const cardHeader = document.createElement('div');
             cardHeader.setAttribute('class', 'card-header');
@@ -15,6 +15,29 @@ $(document).ready(function (){
             const cardBody = document.createElement('div');
             cardBody.setAttribute('class', 'card-body');
             cardBody.innerHTML = '<h5 class="card-title">' + data.title + '</h5><p class="card-text">' + data.year + '</p><p class="card-text">' + data.description + '</p>';
+
+            containerCard.append(cardHeader, cardBody);
+            container.appendChild(containerCard);
+            formalEducationCard.appendChild(container);
+        })
+    }
+    if(nonFormalEducation) {
+        const formalEducationCard = document.getElementById('non-formal-education-card');
+        nonFormalEducation.map(data => {
+            console.log(data);
+            const container = document.createElement('div');
+            container.setAttribute('class', 'col-sm-3');
+
+            const containerCard = document.createElement('div');
+            containerCard.setAttribute('class', 'card text-white bg-primary mb-3 h-100');
+
+            const cardHeader = document.createElement('div');
+            cardHeader.setAttribute('class', 'card-header');
+            cardHeader.innerText = data.head;
+
+            const cardBody = document.createElement('div');
+            cardBody.setAttribute('class', 'card-body');
+            cardBody.innerHTML = '<h5 class="card-title">' + data.title + '</h5><p class="card-text">' + data.year + '</p>';
 
             containerCard.append(cardHeader, cardBody);
             container.appendChild(containerCard);
