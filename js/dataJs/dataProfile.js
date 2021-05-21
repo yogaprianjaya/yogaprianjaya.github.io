c8ed89864e670669fcc77a3e778d7cebb585c9c8
Yoga Prianjaya <yogaprianjaya@gmail.com>
2021-05-21 13:10:43 +0700

    add education data

---
+++ js/dataJs/dataProfile.js
@@ -0,0 +1,58 @@
+const formalEducation = [
+    {
+      "id": 1,
+      "head": "Sekolah Dasar",
+      "title": "SD Negeri Waas 1",
+      "year": "2006 - 2012",
+      "description": "Menyelesaikan pendidikan Sekolah Dasar di SD Negeri Waas 1 secara tepat waktu yaitu selama 6 tahun"
+    },
+    {
+      "id": 2,
+      "head": "Sekolah Menengah Pertama",
+      "title": "SMP Negeri 1 Pameungpeuk",
+      "year": "2012 - 2015",
+      "description": "Menyelesaikan pendidikan Sekolah Menengah Pertama di SMP Negeri 1 Pameungpeuk secara tepat waktu yaitu selama 3 tahun"
+    },
+    {
+      "id": 3,
+      "head": "Sekolah Menengah Atas",
+      "title": "SMK Negeri 2 Baleendah",
+      "year": "2015 - 2018",
+      "description": "Menyelesaikan pendidikan Sekolah Menengah Atas di SMK Negeri 2 Baleendah Jurusan Teknik Komputer & Jaringan secara tepat waktu yaitu selama 3 tahun"
+    },
+    {
+      "id": 4,
+      "head": "Diploma 3",
+      "title": "D3 Teknologi Kompputer",
+      "year": "2018 - Sekarang",
+      "description": "Sedang mengambil kuliah di Program Study D3 Teknologi Komputer, Fakultas Ilmu Terapan, Telkom University"
+    }
+]
+
+const experience = [
+    {
+      "id": 1,
+      "head": "Himpunan Mahasiswa",
+      "year": "2019",
+      "description": "Mengikuti Himpunan Mahasiswa D3 Teknologi Komputer selama 1 Periode sebagai salah satu staff di departemen ahli. Departemen ini bertugas untuk membantu mendorong kemampuan semua mahasiswa KeMa D3 Teknologi Komputer"
+    },
+    {
+      "id": 2,
+      "head": "Asisten Praktikum",
+      "title": "Algoritma & Pemrograman Dasar",
+      "year": "2019",
+      "description": "Menjadi "
+    },
+    {
+      "id": 3,
+      "head": "bebas",
+      "year": "2019 - 2020",
+      "description": ""
+    },
+    {
+      "id": 4,
+      "head": "bebas",
+      "year": "2019 - 2020",
+      "description": ""
+    },
+]
---
+++ js/profile.js
@@ -0,0 +1,24 @@
+$(document).ready(function (){
+    if(formalEducation) {
+        const formalEducationCard = document.getElementById('formal-education-card');
+        formalEducation.map(data => {
+            const container = document.createElement('div');
+            container.setAttribute('class', 'col-3');
+
+            const containerCard = document.createElement('div');
+            containerCard.setAttribute('class', 'card text-white bg-success mb-3');
+
+            const cardHeader = document.createElement('div');
+            cardHeader.setAttribute('class', 'card-header');
+            cardHeader.innerText = data.head;
+
+            const cardBody = document.createElement('div');
+            cardBody.setAttribute('class', 'card-body');
+            cardBody.innerHTML = '<h5 class="card-title">' + data.title + '</h5><p class="card-text">' + data.year + '</p><p class="card-text">' + data.description + '</p>';
+
+            containerCard.append(cardHeader, cardBody);
+            container.appendChild(containerCard);
+            formalEducationCard.appendChild(container);
+        })
+    }
+})
