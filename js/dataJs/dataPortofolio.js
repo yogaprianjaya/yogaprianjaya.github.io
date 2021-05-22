db8ec8567ecf02d40d548ad6cdd0902042453c64
Yoga Prianjaya <yogaprianjaya@gmail.com>
2021-05-23 06:19:11 +0700

    update carousel, card project

---
+++ js/dataJs/dataPortofolio.js
@@ -0,0 +1,32 @@
+const carousel = [
+    {
+        'id': 0,
+        'image': '/asset/img/carousel/carousel-hris.jpg',
+        'alt': 'carousel-hris',
+        'title': 'Aplikasi HRIS',
+        'description': 'Human Resource Information System'
+    },
+    {
+        'id': 1,
+        'image': '/asset/img/carousel/carousel-dummy.jpg',
+        'alt': 'profile',
+        'title': 'title',
+        'description': 'descrtiption lorem lorem'
+    },
+    {
+        'id': 2,
+        'image': '/asset/img/carousel/carousel-dummy.jpg',
+        'alt': 'profile',
+        'title': 'title',
+        'description': 'descrtiption lorem lorem'
+    }
+];
+const cardPortofolio = [
+    {
+        'id': 1,
+        'image': '/asset/img/portofolio/hris/icon.jpg',
+        'name': 'HRIS',
+        'description': 'Human Resource Information System, merupakan aplikasi yang dibuat untuk membantu HRD dalam mengelola staff perusahaan.',
+        'pages': '#'
+    }
+];
